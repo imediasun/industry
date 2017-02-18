88c9a3a967020edf23e0c5cc06bf6e0d7c880f66
Андрей Лопушанский < >
2017-02-19 00:22:38 +0200

    main_page_verstka

--- resources/views/cabinet_page_messages/main_content_view.php
+++ resources/views/cabinet_page_messages/main_content_view.php
@@ -14,7 +14,7 @@
                         </div>
                             <div class="panel">
                                 <div class="panel-heading">
-                                    <h3 class="panel-title">Pagination</h3>
+                                    <h3 class="panel-title">Мої повідомлення</h3>
                                 </div>
 
                                 <!-- Foo Table - Pagination -->
@@ -63,76 +63,68 @@
                                     <table id="demo-foo-pagination" class="table toggle-arrow-small tablet breakpoint footable-loaded footable" data-page-size="5">
                                         <thead>
                                         <tr>
-                                            <th data-toggle="true" class="footable-visible footable-first-column footable-sortable">First Name<span class="footable-sort-indicator"></span></th>
-                                            <th class="footable-visible footable-last-column footable-sortable">Last Name<span class="footable-sort-indicator"></span></th>
-                                            <th data-hide="all" style="display: none;" class="footable-sortable">Job Title<span class="footable-sort-indicator"></span></th>
-                                            <th data-hide="all" style="display: none;" class="footable-sortable">DOB<span class="footable-sort-indicator"></span></th>
+                                            <th data-toggle="true" class="footable-visible footable-first-column footable-sortable">Відправник<span class="footable-sort-indicator"></span></th>
+                                            <th class="footable-visible footable-last-column footable-sortable">Тема листа<span class="footable-sort-indicator"></span></th>
+                                            <th  class="footable-sortable">Дата<span class="footable-sort-indicator"></span></th>
+                                            <th class="footable-sortable">Час<span class="footable-sort-indicator"></span></th>
                                             <th data-hide="all" style="display: none;" class="footable-sortable">Status<span class="footable-sort-indicator"></span></th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr style="display: table-row;" class="footable-even">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Isidra</td>
-                                            <td class="footable-visible footable-last-column">Boudreaux</td>
-                                            <td style="display: none;">Traffic Court Referee</td>
-                                            <td style="display: none;">22 Jun 1972</td>
-                                            <td style="display: none;"><span class="label label-table label-success">Active</span></td>
+                                            <td class="footable-visible footable-last-column"><span class="footable-toggle"></span>Boudreaux</td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
+
                                         </tr>
                                         <tr style="display: table-row;" class="footable-odd">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Shona</td>
                                             <td class="footable-visible footable-last-column">Woldt</td>
-                                            <td style="display: none;">Airline Transport Pilot</td>
-                                            <td style="display: none;">3 Oct 1981</td>
-                                            <td style="display: none;"><span class="label label-table label-dark">Disabled</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: table-row;" class="footable-even">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Granville</td>
                                             <td class="footable-visible footable-last-column">Leonardo</td>
-                                            <td style="display: none;">Business Services Sales Representative</td>
-                                            <td style="display: none;">19 Apr 1969</td>
-                                            <td style="display: none;"><span class="label label-table label-danger">Suspended</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: table-row;" class="footable-odd">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Easer</td>
                                             <td class="footable-visible footable-last-column">Dragoo</td>
-                                            <td style="display: none;">Drywall Stripper</td>
-                                            <td style="display: none;">13 Dec 1977</td>
-                                            <td style="display: none;"><span class="label label-table label-success">Active</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: table-row;" class="footable-even">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Maple</td>
                                             <td class="footable-visible footable-last-column">Halladay</td>
-                                            <td style="display: none;">Aviation Tactical Readiness Officer</td>
-                                            <td style="display: none;">30 Dec 1991</td>
-                                            <td style="display: none;"><span class="label label-table label-danger">Suspended</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: none;" class="footable-odd">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Maxine</td>
                                             <td class="footable-visible footable-last-column"><a href="#">Woldt</a></td>
-                                            <td style="display: none;"><a href="#">Business Services Sales Representative</a></td>
-                                            <td style="display: none;">17 Oct 1987</td>
-                                            <td style="display: none;"><span class="label label-table label-dark">Disabled</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: none;" class="footable-even">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Lorraine</td>
                                             <td class="footable-visible footable-last-column">Mcgaughy</td>
-                                            <td style="display: none;">Hemodialysis Technician</td>
-                                            <td style="display: none;">11 Nov 1983</td>
-                                            <td style="display: none;"><span class="label label-table label-dark">Disabled</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: none;" class="footable-odd">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Lizzee</td>
                                             <td class="footable-visible footable-last-column"><a href="#">Goodlow</a></td>
-                                            <td style="display: none;">Technical Services Librarian</td>
-                                            <td style="display: none;">1 Nov 1961</td>
-                                            <td style="display: none;"><span class="label label-table label-danger">Suspended</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: none;" class="footable-even">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Judi</td>
                                             <td class="footable-visible footable-last-column">Badgett</td>
-                                            <td style="display: none;">Electrical Lineworker</td>
-                                            <td style="display: none;">23 Jun 1981</td>
-                                            <td style="display: none;"><span class="label label-table label-success">Active</span></td>
+                                            <td class="footable-visible footable-last-column">21/02/2017</td>
+                                            <td class="footable-visible footable-last-column">12:30</td>
                                         </tr>
                                         <tr style="display: none;" class="footable-odd">
                                             <td class="footable-visible footable-first-column"><span class="footable-toggle"></span>Lauri</td>
